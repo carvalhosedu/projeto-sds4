a034b57dd72f1a00630255b4f6e45ec2ef14e4c9
Eduardo Luiz Carvalho <carvalhosedu@unochapeco.edu.br>
2021-09-12 12:02:54 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,23 @@
+import Dashboard from 'pages/Dashboard';
+import Home from 'pages/Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+function Routes() {
+    return (
+        <>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" exact>
+                        <Home></Home>
+                    </Route>
+                    <Route path="/">
+                        <Dashboard />
+                    </Route>
+                </Switch>
+            
+            </BrowserRouter>
+        </>
+    );
+}
+
+export default Routes;
